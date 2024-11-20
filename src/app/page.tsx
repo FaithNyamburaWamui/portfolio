b5c8aa49e21bc7527ced51df79da7987ee2c1197
student <faithnyamburawamui@gmail.com>
2024-11-20 09:08:21 +0300

    made changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+// src/app/page.tsx
+import { Jost } from 'next/font/google';
 import Homepage from "./components/Homepage/page";
 import Navbar from "./components/Navbar/page";
 import Projects from "./components/Project/page";
@@ -5,20 +7,21 @@ import Services from "./components/Services/page";
 import Stacks from "./components/Stacks/page";
 import Contact from "./components/Contact/page";
 
+const jost = Jost({
+  subsets: ['latin'],
+  weight: ['400', '700'], // the weights you're using
+  display: 'swap',
+});
+
 export default function Home() {
   return (
-    <main>
-        <link
-          href="https://fonts.googleapis.com/css2?family=Jost:wght@400;700&display=swap"
-          rel="stylesheet"
-        />
-    
-     <Navbar/>
-     <Homepage/>
-     <Services/>
-     <Stacks/>
-     <Projects/>
-     <Contact/>
+    <main className={jost.className}>
+      <Navbar/>
+      <Homepage/>
+      <Services/>
+      <Stacks/>
+      <Projects/>
+      <Contact/>
     </main>
   );
 }
